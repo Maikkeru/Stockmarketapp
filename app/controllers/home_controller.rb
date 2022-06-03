701f93d9faa654a8ec21a6bd5a33c59d89336382
Michael Mitchell <nihongodehanaseru@yahoo.com>
2022-06-03 01:47:09 -0400

    added devise & completed error passes for search bar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,31 @@
 class HomeController < ApplicationController
+   @api = StockQuote::Stock.new(api_key: "pk_153e93f4bce64c3d87e7d4d3510153ed")
   def index
+
+  
+     
+    if params[:ticker] == ""
+      @nothing = "Please enter a ticker Symbol"
+    elsif params[:ticker]
+      begin
+        @stock = StockQuote::Stock.quote(params[:ticker])
+      rescue
+        @stock == 'Unknown symbol'
+      ensure
+        @error = "Please enter a valid stock ticker"
+      end
+    end
+
+
+ end
+
+  def about
   end
+
 end
+
+
+
+
+
+    
